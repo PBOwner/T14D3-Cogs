194001a3b728f23d46641db594d92f906afe0950
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-20 19:01:50 -0400

    Update wormhole.py

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -12,43 +12,49 @@ class WormHole(commands.Cog):
             word_filters=[]
         )  # Initialize the configuration
         self.message_references = {}  # Store message references
+        self.relayed_messages = {}  # Store relayed messages
 
-    async def send_status_message(self, message, channel):
+    async def send_status_message(self, message, channel, title):
         linked_channels = await self.config.linked_channels_list()
         guild = channel.guild
+        embed = discord.Embed(title=title, description=f"{guild.name}: {message}")
         for channel_id in linked_channels:
             relay_channel = self.bot.get_channel(channel_id)
             if relay_channel and relay_channel != channel:
-                await relay_channel.send(f"***The wormhole is shifting...** {guild.name}: {message}*")
+                await relay_channel.send(embed=embed)
 
     @commands.group()
     async def wormhole(self, ctx):
-        """Manage public wormhole connections."""
+        """Manage wormhole connections."""
         pass
 
     @wormhole.command(name="open")
     async def wormhole_open(self, ctx):
-        """Link the current channel to the public wormhole network."""
+        """Link the current channel to the wormhole network."""
         linked_channels = await self.config.linked_channels_list()
         if ctx.channel.id not in linked_channels:
             linked_channels.append(ctx.channel.id)
             await self.config.linked_channels_list.set(linked_channels)
-            await ctx.send("This channel has joined the ever-changing maelstrom that is the public wormhole.")
-            await self.send_status_message(f"A faint signal was picked up from {ctx.channel.mention}, connection has been established.", ctx.channel)
+            embed = discord.Embed(title="Success!", description="This channel has joined the ever-changing maelstrom that is the wormhole.")
+            await ctx.send(embed=embed)
+            await self.send_status_message(f"A faint signal was picked up from {ctx.channel.mention}, connection has been established.", ctx.channel, "Success!")
         else:
-            await ctx.send("This channel is already part of the public wormhole.")
+            embed = discord.Embed(title="ErRoR 404", description="This channel is already part of the wormhole.")
+            await ctx.send(embed=embed)
 
     @wormhole.command(name="close")
     async def wormhole_close(self, ctx):
-        """Unlink the current channel from the public wormhole network."""
+        """Unlink the current channel from the wormhole network."""
         linked_channels = await self.config.linked_channels_list()
         if ctx.channel.id in linked_channels:
             linked_channels.remove(ctx.channel.id)
             await self.config.linked_channels_list.set(linked_channels)
-            await ctx.send("This channel has been severed from the public wormhole.")
-            await self.send_status_message(f"The signal from {ctx.channel.mention} has become too faint to be picked up, the connection was lost.", ctx.channel)
+            embed = discord.Embed(title="Success!", description="This channel has been severed from the wormhole.")
+            await ctx.send(embed=embed)
+            await self.send_status_message(f"The signal from {ctx.channel.mention} has become too faint to be picked up, the connection was lost.", ctx.channel, "Success!")
         else:
-            await ctx.send("This channel is not part of the public wormhole.")
+            embed = discord.Embed(title="ErRoR 404", description="This channel is not part of the wormhole.")
+            await ctx.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -73,17 +79,20 @@ class WormHole(commands.Cog):
 
         if message.channel.id in linked_channels:
             if any(word in message.content for word in word_filters):
-                await message.channel.send("That word is not allowed.")
+                embed = discord.Embed(title="ErRoR 404", description="That word is not allowed.")
+                await message.channel.send(embed=embed)
                 await message.delete()
                 return  # Message contains a filtered word, notify user and delete it
 
             if message.channel.is_nsfw():
-                await message.channel.send("NSFW content is not allowed in the wormhole.")
+                embed = discord.Embed(title="ErRoR 404", description="NSFW content is not allowed in the wormhole.")
+                await message.channel.send(embed=embed)
                 await message.delete()
                 return  # Delete NSFW messages
 
             if "@everyone" in message.content or "@here" in message.content:
-                await message.channel.send("`@everyone` and `@here` pings are not allowed.")
+                embed = discord.Embed(title="ErRoR 404", description="`@everyone` and `@here` pings are not allowed.")
+                await message.channel.send(embed=embed)
                 await message.delete()
                 return  # Message contains prohibited pings, notify user and delete it
 
@@ -99,13 +108,14 @@ class WormHole(commands.Cog):
                     if channel:
                         if message.attachments:
                             for attachment in message.attachments:
-                                await channel.send(f"**{message.guild.name} - {display_name}:** {message.content}")
+                                relay_message = await channel.send(embed=discord.Embed(title=f"{message.guild.name} - {display_name}", description=message.content))
                                 await attachment.save(f"temp_{attachment.filename}")
                                 with open(f"temp_{attachment.filename}", "rb") as file:
                                     await channel.send(file=discord.File(file))
                                 os.remove(f"temp_{attachment.filename}")
                         else:
-                            await channel.send(f"**{message.guild.name} - {display_name}:** {message.content}")
+                            relay_message = await channel.send(embed=discord.Embed(title=f"{message.guild.name} - {display_name}", description=message.content))
+                        self.relayed_messages[(message.id, channel_id)] = relay_message.id
 
             # Check for mentions and send DM if it's a reply
             if message.reference and message.reference.message_id in self.message_references:
@@ -116,8 +126,17 @@ class WormHole(commands.Cog):
                     embed = discord.Embed(title="You were mentioned")
                     embed.add_field(name="Who", value=message.author.mention, inline=False)
                     embed.add_field(name="Where", value=f"{message.channel.mention} in {message.guild.name}", inline=False)
-                    embed.add_field(name="What", value=message.content, inline=False)
+                    embed.add_field(name="When", value=message.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
                     await original_author.send(embed=embed)
+
+            # Check for direct mentions and send DM
+            for user in message.mentions:
+                if user.id != message.author.id:
+                    embed = discord.Embed(title="You were mentioned")
+                    embed.add_field(name="Who", value=message.author.mention, inline=False)
+                    embed.add_field(name="Where", value=f"{message.channel.mention} in {message.guild.name}", inline=False)
+                    embed.add_field(name="When", value=message.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+                    await user.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message_edit(self, before: discord.Message, after: discord.Message):
@@ -132,7 +151,12 @@ class WormHole(commands.Cog):
                 if channel_id != after.channel.id:
                     channel = self.bot.get_channel(channel_id)
                     if channel:
-                        await channel.send(f"**{after.guild.name} - {display_name} (edited):** {after.content}")
+                        if (before.id, channel_id) in self.relayed_messages:
+                            relay_message_id = self.relayed_messages[(before.id, channel_id)]
+                            relay_message = await channel.fetch_message(relay_message_id)
+                            await relay_message.delete()
+                            new_relay_message = await channel.send(embed=discord.Embed(title=f"{after.guild.name} - {display_name} (edited)", description=after.content))
+                            self.relayed_messages[(after.id, channel_id)] = new_relay_message.id
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
@@ -141,72 +165,83 @@ class WormHole(commands.Cog):
 
         linked_channels = await self.config.linked_channels_list()
 
-        # Check if the message is in a public wormhole channel
+        # Check if the message is in a wormhole channel
         if message.channel.id in linked_channels:
             for channel_id in linked_channels:
                 if channel_id != message.channel.id:
                     channel = self.bot.get_channel(channel_id)
-                    if channel:
-                        async for msg in channel.history(limit=100):
-                            if msg.content == f"**{message.guild.name} - {message.author.display_name}:** {message.content}":
-                                await msg.delete()
-                                break
+                    if channel and (message.id, channel_id) in self.relayed_messages:
+                        relay_message_id = self.relayed_messages[(message.id, channel_id)]
+                        relay_message = await channel.fetch_message(relay_message_id)
+                        await relay_message.delete()
 
     @wormhole.command(name="globalblacklist")
     async def wormhole_globalblacklist(self, ctx, user: discord.User):
-        """Prevent specific members from sending messages through the public wormhole globally."""
+        """Prevent specific members from sending messages through the wormhole globally."""
         if await self.bot.is_owner(ctx.author):
             global_blacklist = await self.config.global_blacklist()
             if user.id not in global_blacklist:
                 global_blacklist.append(user.id)
                 await self.config.global_blacklist.set(global_blacklist)
-                await ctx.send(f"{user.display_name} has been added to the global public wormhole blacklist.")
+                embed = discord.Embed(title="Success!", description=f"{user.display_name} has been added to the global wormhole blacklist.")
+                await ctx.send(embed=embed)
             else:
-                await ctx.send(f"{user.display_name} is already in the global public wormhole blacklist.")
+                embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is already in the global wormhole blacklist.")
+                await ctx.send(embed=embed)
         else:
-            await ctx.send("You must be the bot owner to use this command.")
+            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
+            await ctx.send(embed=embed)
 
     @wormhole.command(name="unglobalblacklist")
     async def wormhole_unglobalblacklist(self, ctx, user: discord.User):
-        """Command to remove a user from the global public wormhole blacklist (Bot Owner Only)."""
+        """Command to remove a user from the global wormhole blacklist (Bot Owner Only)."""
         if await self.bot.is_owner(ctx.author):
             global_blacklist = await self.config.global_blacklist()
             if user.id in global_blacklist:
                 global_blacklist.remove(user.id)
                 await self.config.global_blacklist.set(global_blacklist)
-                await ctx.send(f"{user.display_name} has been removed from the global public wormhole blacklist.")
+                embed = discord.Embed(title="Success!", description=f"{user.display_name} has been removed from the global wormhole blacklist.")
+                await ctx.send(embed=embed)
             else:
-                await ctx.send(f"{user.display_name} is not in the global public wormhole blacklist.")
+                embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is not in the global wormhole blacklist.")
+                await ctx.send(embed=embed)
         else:
-            await ctx.send("You must be the bot owner to use this command.")
+            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
+            await ctx.send(embed=embed)
 
     @wormhole.command(name="addwordfilter")
     async def wormhole_addwordfilter(self, ctx, *, word: str):
-        """Add a word to the public wormhole word filter."""
+        """Add a word to the wormhole word filter."""
         if await self.bot.is_owner(ctx.author):
             word_filters = await self.config.word_filters()
             if word not in word_filters:
                 word_filters.append(word)
                 await self.config.word_filters.set(word_filters)
-                await ctx.send(f"`{word}` has been added to the public wormhole word filter.")
+                embed = discord.Embed(title="Success!", description=f"`{word}` has been added to the wormhole word filter.")
+                await ctx.send(embed=embed)
             else:
-                await ctx.send(f"`{word}` is already in the public wormhole word filter.")
+                embed = discord.Embed(title="ErRoR 404", description=f"`{word}` is already in the wormhole word filter.")
+                await ctx.send(embed=embed)
         else:
-            await ctx.send("You must be the bot owner to use this command.")
+            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
+            await ctx.send(embed=embed)
 
     @wormhole.command(name="removewordfilter")
     async def wormhole_removewordfilter(self, ctx, *, word: str):
-        """Remove a word from the public wormhole word filter."""
+        """Remove a word from the wormhole word filter."""
         if await self.bot.is_owner(ctx.author):
             word_filters = await self.config.word_filters()
             if word in word_filters:
                 word_filters.remove(word)
                 await self.config.word_filters.set(word_filters)
-                await ctx.send(f"`{word}` has been removed from the public wormhole word filter.")
+                embed = discord.Embed(title="Success!", description=f"`{word}` has been removed from the wormhole word filter.")
+                await ctx.send(embed=embed)
             else:
-                await ctx.send(f"`{word}` is not in the public wormhole word filter.")
+                embed = discord.Embed(title="ErRoR 404", description=f"`{word}` is not in the wormhole word filter.")
+                await ctx.send(embed=embed)
         else:
-            await ctx.send("You must be the bot owner to use this command.")
+            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(WormHole(bot))
